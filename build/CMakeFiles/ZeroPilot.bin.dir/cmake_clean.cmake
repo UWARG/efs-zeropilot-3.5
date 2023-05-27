98682213ae02636fc481c2e377e6b328b4158f54
Christopher Chung <tps90067@gmail.com>
2023-05-27 14:46:51 -0500

    delete build folder

--- build/CMakeFiles/ZeroPilot.bin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ZeroPilot.bin"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ZeroPilot.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
