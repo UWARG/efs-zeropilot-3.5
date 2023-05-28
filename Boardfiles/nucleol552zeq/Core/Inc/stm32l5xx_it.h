d12cc6ae9164b28eba3efc59be945eea1020258d
Christopher Chung <tps90067@gmail.com>
2023-05-28 15:00:57 -0500

    Change system timer to SysTick

--- Boardfiles/nucleol552zeq/Core/Inc/stm32l5xx_it.h
+++ Boardfiles/nucleol552zeq/Core/Inc/stm32l5xx_it.h
@@ -57,7 +57,6 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void DMA1_Channel1_IRQHandler(void);
 void DMA1_Channel2_IRQHandler(void);
-void TIM3_IRQHandler(void);
 void TIM5_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
--- Boardfiles/nucleol552zeq/Core/Src/stm32l5xx_it.c
+++ Boardfiles/nucleol552zeq/Core/Src/stm32l5xx_it.c
@@ -58,8 +58,6 @@
 extern DMA_HandleTypeDef hdma_tim5_ch1;
 extern DMA_HandleTypeDef hdma_tim5_ch2;
 extern TIM_HandleTypeDef htim5;
-extern TIM_HandleTypeDef htim3;
-
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -189,7 +187,7 @@ void SysTick_Handler(void)
   /* USER CODE BEGIN SysTick_IRQn 0 */
 
   /* USER CODE END SysTick_IRQn 0 */
-
+  HAL_IncTick();
   /* USER CODE BEGIN SysTick_IRQn 1 */
 
   /* USER CODE END SysTick_IRQn 1 */
@@ -228,20 +226,6 @@ void DMA1_Channel2_IRQHandler(void)
   /* USER CODE BEGIN DMA1_Channel2_IRQn 1 */
 
   /* USER CODE END DMA1_Channel2_IRQn 1 */
-}
-
-/**
-  * @brief This function handles TIM3 global interrupt.
-  */
-void TIM3_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM3_IRQn 0 */
-
-  /* USER CODE END TIM3_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim3);
-  /* USER CODE BEGIN TIM3_IRQn 1 */
-
-  /* USER CODE END TIM3_IRQn 1 */
 }
 
 /**
