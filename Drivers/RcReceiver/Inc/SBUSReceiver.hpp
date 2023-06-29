a7c2986873faaedff4ee9a87e83a9aa52f2c68ca
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2023-06-29 00:20:18 -0400

    port sbusReceiver, remove singleton

---
+++ Drivers/RcReceiver/Inc/SBUSReceiver.hpp
@@ -0,0 +1,62 @@
+#ifndef SBUS_RECEIVER_HPP
+#define SBUS_RECEIVER_HPP
+
+#include "main.h"
+#include "usart.h"
+#include "CommonDataTypes.h"
+#include "SBUSCommonDefines.h"
+
+/*
+ * UART Config for this sbus driver
+ * Word Length - 9 bits(Including Parity)
+ * Parity - Even
+ * Stop Bits - 2
+ * TX&RX Pin Activitive Level Inversion - enabled (this can depend on the type of sbus)
+ * enable dma interrupt for uart_rx
+ * don't forget to enable uart IRQ
+ */
+
+class SBUSReceiver{
+    public:
+    	SBUSReceiver(UART_HandleTypeDef* uart);
+
+    /*
+        get the sbus data
+        @return SBus struct
+    */
+        SBus GetSBUS();
+
+    /*
+        get the RCControl data that is parsed from sbus
+        more readable data that varies from 0 to 100
+        @return RCControl struct
+    */
+        RCControl GetRCControl();
+
+        void parse();
+    /*
+        public variable
+    */
+        uint8_t raw_sbus_[SBUS_FRAME_SIZE];
+
+    private:
+
+    /*
+        object constructor
+        @param uart instance ie. &huart1
+    */
+
+    // member variables
+        UART_HandleTypeDef* uart_;
+        SBus received_sbus_;
+        RCControl received_rccontrol_;
+
+    //private functions
+        void read();
+        void cast_rccontrol();
+        float sbus_to_rccontrol(uint16_t channel_value);
+
+};
+
+
+#endif
