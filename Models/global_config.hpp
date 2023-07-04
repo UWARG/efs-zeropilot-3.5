bdea61fb551213a9013933c4e0a787ae30ec5484
Anthony Bertnyk <anthony.bertnyk@gmail.com>
2023-07-04 19:36:34 -0400

    Improve config structure and implement driver factory function

--- Models/global_config.hpp
+++ Models/global_config.hpp
@@ -1,51 +1,77 @@
+#ifndef ZPSW3_GLOBAL_CONFIG_HPP
+#define ZPSW3_GLOBAL_CONFIG_HPP
+
 #include <cstdint>
 
-typedef enum {
-    yaw,
-    pitch,
-    roll,
-    thrust
-} ControlAxis_t;
+namespace config
+{
 
-typedef enum {
-    positive,
-    negative
-} ControlDirection_t;
+    typedef enum {
+        yaw,
+        pitch,
+        roll,
+        thrust
+    } ControlAxis_t;
 
-//TODO: Determine correct location for motor driver definitions, and finish implementation.
-class MotorDriver{};
-class TempPWMDriver : public MotorDriver{};
-class TempDSHOTDriver : public MotorDriver{};
+    //TODO: Determine correct location for motor driver definitions, and finish implementation.
+    class MotorDriver{};
+    class TempPWMDriver : public MotorDriver{};
+    class TempDSHOTDriver : public MotorDriver{};
 
-typedef struct {
-    ControlAxis_t axis;
-    ControlDirection_t direction;
-    MotorDriver driver;
-    //TODO: Consider a more specific datatype for interfaceID
-    //(e.g. a datatype defined within the motor driver class)
-    uint8_t interfaceID;
-} Motor_t;
+    //Define factory function to create motor drivers.
+    //The driver class must be copy-constructable as the returned object will be a copy.
+    template <class DriverType>
+    MotorDriver constructDriver() {
+        DriverType driver;
+        return driver;
+    }
 
-typedef struct {
-    float p;
-    float i;
-    float d;
-} PID_t;
+    //Typedef pointer to factory function
+    typedef MotorDriver (*DriverFactory)(void);
 
-typedef struct {
-    float min;
-    float max;
-} AxisLimit_t;
+    typedef struct {
+        ControlAxis_t axis;
+        bool isInverted;
+        DriverFactory driverConstructor;
+        //TODO: Consider a more specific datatype for interfaceID
+        //(e.g. a datatype defined within the motor driver class)
+        uint8_t interfaceID;
+    } Motor_t;
 
-typedef struct {
-    AxisLimit_t yawLimit;
-    AxisLimit_t pitchLimit;
-    AxisLimit_t rollLimit;
-    AxisLimit_t thrustLimit;
-} ControlLimits_t;
+    typedef struct {
+        bool isEnabled;
+        float p;
+        float i;
+        float d;
+    } AxisPID_t;
 
-//TODO: Combine flightmode classes with AM control algorithms
-class Flightmode{
-    PID_t PIDValues;
-    ControlLimits_t controlLimits;
-};
+    typedef struct {
+        AxisPID_t yawPID;
+        AxisPID_t pitchPID;
+        AxisPID_t rollPID;
+        AxisPID_t thrustPID;
+    } ControlPID_t;
+
+    typedef struct {
+        float min;
+        float max;
+    } AxisLimits_t;
+
+    typedef struct {
+        AxisLimits_t yawLimit;
+        AxisLimits_t pitchLimit;
+        AxisLimits_t rollLimit;
+        AxisLimits_t thrustLimit;
+    } ControlLimits_t;
+
+    //TODO: Combine flightmode classes with AM control algorithms
+    class Flightmode{
+        public:
+        ControlPID_t PIDValues_;
+        ControlLimits_t controlLimits_;
+        constexpr Flightmode(ControlPID_t pid, ControlLimits_t limits): PIDValues_(pid), controlLimits_(limits){}
+    };
+
+}
+
+#endif // ZPSW3_GLOBAL_CONFIG_HPP
