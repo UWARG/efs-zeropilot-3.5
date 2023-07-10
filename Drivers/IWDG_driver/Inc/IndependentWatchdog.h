c97c7d43c634f79507b9e690fc6f53ad73256690
Pratyush Makkar <pratyushmakkar@gmail.com>
2023-07-10 19:24:17 -0300

    Finished testing IWDG

--- Drivers/IWDG_driver/Inc/IndependentWatchdog.h
+++ Drivers/IWDG_driver/Inc/IndependentWatchdog.h
@@ -8,20 +8,22 @@
 #ifndef SRC_DRIVERS_INDEPENDENTWATCHDOG_H_
 #define SRC_DRIVERS_INDEPENDENTWATCHDOG_H_
 
+
 #include <stdint.h>
-#include "../../boardfiles/discoveryl562qe/Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_iwdg.h"
+#include "main.h"
 
 class IWDG_t {
 	private:
 		uint8_t iwdg_prescaler;
-		uint32_t iwdg_reload;
+		uint16_t iwdg_reload;
 
 	public:
 		IWDG_t(IWDG_HandleTypeDef* watchdog);
 		IWDG_t(uint8_t iwdg_prescaler, uint32_t iwdg_reload);
-		HAL_StatusTypeDef InitWatchdog(IWDG_HandleTypeDef* watchdog, IWDG_t* cfg);
+		HAL_StatusTypeDef InitWatchdog(IWDG_HandleTypeDef* watchdog);
 		static HAL_StatusTypeDef RefreshWatchdog(IWDG_HandleTypeDef* watchdog);
 };
 
 
 #endif /* SRC_DRIVERS_INDEPENDENTWATCHDOG_H_ */
+
