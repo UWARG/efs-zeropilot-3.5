578e327b3b69f43836970977a17622a98dd19003
Le Kang Wang <70457170+lekangwang@users.noreply.github.com>
2023-07-27 22:24:35 -0400

    AM input interface (#16)

--- AttitudeManager/Inc/AM.hpp
+++ AttitudeManager/Inc/AM.hpp
@@ -12,10 +12,17 @@
 #include "AM_ControlAlgorithm.hpp"
 #include "CommonDataTypes.hpp"
 
+#include "FreeRTOS.h"
+#include "semphr.h"
+
 namespace AM {
 
 class AttitudeManager {
    public:
+    static void setControlInputs(const AttitudeManagerInput& new_control_inputs);
+
+    static AttitudeManagerInput getControlInputs();
+
     AttitudeManager(ControlAlgorithm* control_algorithm) : control_algorithm(control_algorithm){};
 
     void runControlLoopIteration(const AttitudeManagerInput& instructions);
@@ -23,8 +30,13 @@ class AttitudeManager {
    private:
     AttitudeManager();
 
+    static SemaphoreHandle_t control_inputs_mutex;
+
+    static struct AttitudeManagerInput control_inputs;
+
     ControlAlgorithm* control_algorithm;
 };
+
 }  // namespace AM
 
 #endif  // ZPSW3_AM_HPP
--- AttitudeManager/Src/AM.cpp
+++ AttitudeManager/Src/AM.cpp
@@ -3,12 +3,39 @@
 //
 #include "AM.hpp"
 
+#include "FreeRTOS.h"
+#include "semphr.h"
+
 #include <array>
 #include <cstdlib>
 
 namespace AM {
+SemaphoreHandle_t AttitudeManager::control_inputs_mutex = xSemaphoreCreateMutex();
 
-void AttitudeManager::runControlLoopIteration(const AttitudeManagerInput &instructions) {
+AttitudeManagerInput AttitudeManager::control_inputs = {
+    .roll = 0.0f,
+    .pitch = 0.0f,
+    .yaw = 0.0f,
+    .throttle = 0.0f
+};
+
+void AttitudeManager::setControlInputs(const AttitudeManagerInput& new_control_inputs) {
+    if (xSemaphoreTake(control_inputs_mutex, (TickType_t) portMAX_DELAY) == pdPASS) {
+        control_inputs = new_control_inputs;
+        xSemaphoreGive(control_inputs_mutex);
+    }
+}
+
+AttitudeManagerInput AttitudeManager::getControlInputs() {
+    AttitudeManagerInput temp {};
+    if (xSemaphoreTake(control_inputs_mutex, (TickType_t) portMAX_DELAY) == pdPASS) {
+        temp = control_inputs;
+        xSemaphoreGive(control_inputs_mutex);
+    }
+    return temp;
+}
+
+void AttitudeManager::runControlLoopIteration(const AttitudeManagerInput& instructions) {
     // Process Instructions
 
     // Run Control Algorithms
