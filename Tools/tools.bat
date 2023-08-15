608edbf8e03d5a29f86f92a6a1ace170922f9416
DerekTang04 <tang.derek04@gmail.com>
2023-08-15 03:59:36 -0400

    compiling works

--- Tools/tools.bat
+++ Tools/tools.bat
@@ -1,7 +1,10 @@
-@rem main script start
 @echo off
-setlocal enabledelayedexpansion
 
+@rem ==================
+@rem --- get inputs ---
+@rem ==================
+
+setlocal enabledelayedexpansion
 
 @rem read config.txt
 set /a is_multiline=0
@@ -16,7 +19,7 @@ for /f "tokens=*" %%A in (%~dp0config.txt) do (
         for /f "tokens=1 delims=#" %%B in ("!raw_input!") do set clean_input=%%B
         
         @rem process cleaned input line
-        if [!is_multiline!]==[0] (
+        if !is_multiline! equ 0 (
             @rem single line mode
             @rem read key-value pair
             for /f "tokens=1,2 delims==" %%C in ("!clean_input!") do (
@@ -48,7 +51,6 @@ for /f "tokens=*" %%A in (%~dp0config.txt) do (
 )
 call :clean_vars is_multiline raw_input char0 clean_input key value
 
-
 @rem read mode
 if "%~1"=="" call :throw_err "missing mode" & goto :exit_err
 if "%~1"=="frm" set mode=firmware
@@ -56,7 +58,6 @@ if "%~1"=="firmware" set mode=firmware
 if "%~1"=="tst" set mode=testing
 if "%~1"=="testing" set mode=testing
 if "%mode%"=="" call :throw_err "unknown mode '%~1'" & goto :exit_err
-
 
 @rem read operations
 if "%~2"=="" call :throw_err "missing operations" & goto :exit_err
@@ -79,10 +80,9 @@ for %%A in (%operations%) do (
        call :set_operations do_run
        if not "%mode%"=="testing" call :throw_warn "redundant operation 'r' - running is only supported in testing mode"
     )
-    if [!is_valid!]==[0] call :throw_err "invalid operation '%%A'" & goto :exit_err
+    if !is_valid! equ 0 call :throw_err "invalid operation '%%A'" & goto :exit_err
 )
 call :clean_vars operations is_valid
-
 
 @rem read options
 for /f "tokens=1-2*" %%A in ("%*") do set opts=%%C
@@ -90,7 +90,7 @@ if "%opts%"=="" goto :start_work
 
 set /a is_key=1
 for %%A in (%opts%) do (
-    if [!is_key!]==[1] (
+    if !is_key! equ 1 (
         set key=
         if "%%A"=="-p" call :set_options PLATFORM firmware "redundant option '-p' - platform select is only supported in firmware mode"
         if "%%A"=="-e" call :set_options LINT_EXCLUSION firmware "redundant option '-e' - lint exclusion is only supported in firmware mode"
@@ -104,18 +104,92 @@ for %%A in (%opts%) do (
 )
 call :clean_vars opts is_key key
 
+@rem ==================
+@rem --- start work ---
+@rem ==================
 
 :start_work
-@rem do the things
+@rem find cmake generator
+where ninja >nul 2>nul && set generator="Ninja" && goto :generator_found
+where make >nul 2>nul && set generator="Unix Makefiles" && goto :generator_found
+where mingw32-make >nul 2>nul && set generator="MinGW Makefiles" && goto :generator_found
+call :throw_err "no cmake generator found" & goto :exit_err
+:generator_found
+set "generator=%generator:~0,-1%"
 
+echo Script is running in %mode% mode
+set bin_dir=%~dp0%mode%/build
 
+@rem clean build artifacts
+if %do_clean% equ 1 (
+    echo Cleaning old %mode% build environment
+    cmake -E rm -rf "%bin_dir%" 
+) 
 
-@rem main script end
+@rem core operations
+cmake -E make_directory %bin_dir%
+if "%mode%"=="firmware" (
+    @rem create build system if it does not exist
+    if not exist "%bin_dir%/cmakecache.txt" (
+        echo\
+        echo Configuring for %PLATFORM%
+        echo Creating %generator:"=% build system...
+        cmake -E chdir %bin_dir%^
+         cmake -G %generator% -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_TOOLCHAIN_FILE="../../../Boardfiles/%PLATFORM%/%PLATFORM%.cmake" ..
+        if not %ERRORLEVEL% equ 0 call :throw_err "Failed to create %generator:"=% build system" & goto :exit_err
+    )
+
+    @rem compile
+    if %do_compile% equ 1 (
+        echo\
+        echo Compiling project...
+        cmake -E chdir %bin_dir% cmake --build .
+        if %ERRORLEVEL% equ 0 (
+            echo Successfully compiled Zeropilot for firmware
+        ) else (
+            call :throw_err "Failed to compile ZeroPilot firmware project" & goto :exit_err
+        )
+    )
+    
+    @rem lint
+    if %do_lint% equ 1 (
+        echo\
+        echo Linting project with clang-tidy...
+
+        echo\
+        echo Checking project format with clang-format...
+    )
+) else (
+    @rem compile
+    if %do_compile% equ 1 (
+        echo\
+        echo Creating %generator:"=% build system...
+        cmake -E chdir %bin_dir% cmake -G %generator% ..
+        if not !ERRORLEVEL! equ 0 call :throw_err "Failed to create %generator:"=% build system" & goto :exit_err
+
+        echo\
+        echo Compiling project...
+        cmake -E chdir %bin_dir% cmake --build .
+        if !ERRORLEVEL! equ 0 (
+            echo Successfully compiled Zeropilot for testing
+        ) else (
+            call :throw_err "Failed to compile ZeroPilot testing project" & goto :exit_err
+        )
+    )
+
+    @rem run
+    if %do_run% equ 1 (
+        echo\
+    )
+)
+
 endlocal
 exit /b 0
 
+@rem ========================
+@rem --- helper functions ---
+@rem ========================
 
-@rem helper functions
 :set_operations
 set /a is_valid=1
 for %%A in (%*) do set /a %%A=1
