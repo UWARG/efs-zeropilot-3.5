289e79d0a54ef8033e6c66006ea5ac722b9cd0a6
Chris Chung <69175242+Chrisc110@users.noreply.github.com>
2023-11-15 21:24:16 -0500

    Basic sm failsafe (#38)
    Co-authored-by: Anthony Bertnyk <anthony.bertnyk@gmail.com>
    Co-authored-by: TheSpaceDragon <developerskytech@gmail.com>
    Co-authored-by: Navtaj Hundal <n2hundal@uwaterloo.ca>
    Co-authored-by: unknown <aeun@uwaterloo.ca>
    Co-authored-by: WARG-Laptop <uw.warg@gmail.com>
    Co-authored-by: Aidan Bowers <abowers@uwaterloo.ca>
    Co-authored-by: Hardy <yudonghe0120@outlook.com>

--- Boardfiles/nucleol552zeq/Core/Src/dma.c
+++ Boardfiles/nucleol552zeq/Core/Src/dma.c
@@ -53,6 +53,9 @@ void MX_DMA_Init(void)
   /* DMA1_Channel3_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel3_IRQn, 5, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel3_IRQn);
+  /* DMA1_Channel4_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel4_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel4_IRQn);
 
 }
 
