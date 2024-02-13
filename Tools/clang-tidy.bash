326aecc6003e3dff67f9c127b2eca20b6fcb9a34
Stanley <stang0926@gmail.com>
2024-02-12 21:03:04 -0800

    Rename clang-tidy script and add ignore paths

--- Tools/clang-tidy.bash
+++ Tools/clang-tidy.bash
@@ -1,20 +1,29 @@
+#!/usr/bin/env bash
+
 # define path to this bash script
 SCRIPT_PATH=$(dirname -- "$0")
 
-# grab source file paths from compile_commands.json (excluding Boardfiles)
-SRC_FILES=($(grep -e "\"file\": \"/src/.*\.cp*" $SCRIPT_PATH/Firmware/build/compile_commands.json | grep -v "Boardfiles"))
+# grab source file paths from compile_commands.json
+SRC_FILES=($(grep -e "\"file\": \"/src/.*\.cp*" $SCRIPT_PATH/Firmware/build/compile_commands.json))
 SRC_FILES+=($(grep -e "\"file\":.*main.cpp" $SCRIPT_PATH/Firmware/build/compile_commands.json))
 # filter none file path strings
-SRC_FILES=($(printf "%s\n" "${SRC_FILES[@]}" | grep -v 'file'))
+SRC_FILES=($(printf "%s\n" "${SRC_FILES[@]}" | grep -v '"file"'))
+# exclude file/folder paths specified in clang-tidy_ignore.txt
+while IFS=$'\n' read -r line; do
+    if [[ $line != *#* && $line != "" ]]; then
+        SRC_FILES=($(printf "%s\n" "${SRC_FILES[@]}" | grep -v "${line}"))
+    fi
+done < "$SCRIPT_PATH/clang-tidy_ignore.txt"
 # trim quotation marks in file path string
 for i in "${!SRC_FILES[@]}"; do
     SRC_FILES[$i]=${SRC_FILES[$i]:1:-1}
 done
 
+
 COMPILER_ROOT=/usr/share/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi
 for SRC_FILE in "${SRC_FILES[@]}"
 do
-    echo "clang-tidy ${SRC_FILE}"
+    echo "Analyzing ${SRC_FILE}"
     clang-tidy --checks=-*,cppcoreguidelines-avoid-do-while \
         -p /src/Tools/Firmware/build \
         --extra-arg=--target=arm-none-eabi \
