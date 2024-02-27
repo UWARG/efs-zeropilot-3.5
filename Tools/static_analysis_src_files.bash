988e21e849da6faf1266df9f34b1fdf2bb74725d
Stanley <stang0926@gmail.com>
2024-02-26 22:44:42 -0800

    Add headers to static analysis

--- Tools/static_analysis_src_files.bash
+++ Tools/static_analysis_src_files.bash
@@ -3,20 +3,23 @@
 # define path to this bash script
 SCRIPT_PATH=$(dirname -- "$0")
 
-# grab source file paths from compile_commands.json
-SRC_FILES=($(grep -e "\"file\": \"/src/.*\.cp*" $SCRIPT_PATH/Firmware/build/compile_commands.json))
-SRC_FILES+=($(grep -e "\"file\":.*main.cpp" $SCRIPT_PATH/Firmware/build/compile_commands.json))
-# filter none file path strings
-SRC_FILES=($(printf "%s\n" "${SRC_FILES[@]}" | grep -v '"file"'))
-# exclude file/folder paths specified in clang-tidy_ignore.txt
+# exclude paths specified in static_analysis_ignore.txt
+PRUNE_PATHS="\( "
 while IFS=$'\n' read -r line; do
     if [[ $line != *#* && $line != "" ]]; then
-        SRC_FILES=($(printf "%s\n" "${SRC_FILES[@]}" | grep -v "${line}"))
+        PRUNE_PATHS+="-path /src/${line} -o "
     fi
 done < "$SCRIPT_PATH/static_analysis_ignore.txt"
-# trim quotation marks in file path string
-for i in "${!SRC_FILES[@]}"; do
-    SRC_FILES[$i]=${SRC_FILES[$i]:1:-1}
-done
+PRUNE_PATHS=${PRUNE_PATHS:0:-3}
+PRUNE_PATHS+="\)"
+# construct search command
+FIND_CMD="find /src $PRUNE_PATHS -prune -o -print"
+GREP_CMD="grep \"\.\(c\|h\)\(pp\)*$\" | grep -v Test" # exclude test files
+SEARCH_CMD="$FIND_CMD | $GREP_CMD"
+
+# search for source and header files
+SRC_FILES=($(eval $SEARCH_CMD))
+# add main.cpp
+SRC_FILES+=($(find /src -name "*main.cpp"))
 
 echo "${SRC_FILES[@]}"
