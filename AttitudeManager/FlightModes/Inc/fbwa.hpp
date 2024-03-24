dbeb2d9042e2c8f48e7354bde5984153e3b9608a
alyzenjeraj <alyzenjeraj@gmail.com>
2024-03-23 22:17:58 -0400

    based of Aidans PR

---
+++ AttitudeManager/FlightModes/Inc/fbwa.hpp
@@ -0,0 +1,18 @@
+#ifndef ZPSW3_AM_FBWA_HPP
+#define ZPSW3_AM_FBWA_HPP
+
+#include "flightmode.hpp"
+
+namespace AM {
+
+class FBWA : public Flightmode {
+   public:
+    FBWA() = default;
+
+    AttitudeManagerInput run(const AttitudeManagerInput& input) override;
+    void updatePid() override;
+};
+
+}  // namespace AM
+
+#endif  // ZPSW3_AM_FBWA_HPP
---
+++ AttitudeManager/FlightModes/Src/fbwa.cpp
@@ -0,0 +1,10 @@
+#include "fbwa.hpp"
+#include "CommonDataTypes.hpp"
+
+namespace AM {
+    AttitudeManagerInput FBWA::run(const AttitudeManagerInput& input) {
+        return input;
+    }
+
+    void FBWA::updatePid() {}
+}
