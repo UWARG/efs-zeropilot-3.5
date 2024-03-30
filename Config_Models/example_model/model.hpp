04c0ba34db94eeb03ef10ade5ce25c7c7768a8ff
alyzenjeraj <alyzenjeraj@gmail.com>
2024-03-29 20:52:15 -0400

    Removed minus typo

--- Config_Models/example_model/model.hpp
+++ Config_Models/example_model/model.hpp
@@ -119,7 +119,7 @@ namespace config
                     },
                     .pitchLimit = {
                         .min = -30.0f,
-                        .max = -30.0f
+                        .max = 30.0f
                     }
                 }
             },
