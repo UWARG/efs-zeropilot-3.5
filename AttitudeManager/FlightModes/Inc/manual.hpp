2c7774493bed4b6576715f823158db6d730ca6bd
Camron Sabahi <csabahip@uwaterloo.ca>
2024-03-30 13:30:01 -0400

    updatePID in fbwa, and fixed compile errors in PID.hpp

--- AttitudeManager/FlightModes/Inc/manual.hpp
+++ AttitudeManager/FlightModes/Inc/manual.hpp
@@ -9,7 +9,7 @@ class Manual : public Flightmode {
    public:
     Manual() = default;
 
-    AttitudeManagerInput run(const AttitudeManagerInput& input) override;
+    AttitudeManagerInput run(const AttitudeManagerInput& input, AxisPIDs _pids) override;
     void updatePid(PIDController _axis, GainTerm whichGain, float desiredGain ) override;
     void updatePidGains() override;
     void updateControlLimits(ControlLimits_t limits) override;
