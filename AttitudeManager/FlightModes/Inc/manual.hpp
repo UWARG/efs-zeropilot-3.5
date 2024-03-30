a1ceef41a53c003cbc0e0e8e61bcd432ae6f87f0
Camron Sabahi <csabahip@uwaterloo.ca>
2024-03-30 12:13:00 -0400

    Update PID gains in manual

--- AttitudeManager/FlightModes/Inc/manual.hpp
+++ AttitudeManager/FlightModes/Inc/manual.hpp
@@ -9,7 +9,7 @@ class Manual : public Flightmode {
    public:
     Manual() = default;
 
-    AttitudeManagerInput run(const AttitudeManagerInput& input, AxisPIDs _pids) override;
+    AttitudeManagerInput run(const AttitudeManagerInput& input) override;
     void updatePid(PIDController _axis, GainTerm whichGain, float desiredGain ) override;
     void updatePidGains() override;
     void updateControlLimits(ControlLimits_t limits) override;
