19ee99d2897ddeb2275fecb976408fee8d45c5c7
alyzenjeraj <alyzenjeraj@gmail.com>
2024-03-29 22:24:18 -0400

    changed mapping functions

--- AttitudeManager/FlightModes/Src/fbwa.cpp
+++ AttitudeManager/FlightModes/Src/fbwa.cpp
@@ -9,9 +9,7 @@ namespace AM {
         
         AttitudeManagerInput mappedOutputs;
 
-        assert(AM::AttitudeManager::INPUT_MAX == 100 && AM::AttitudeManager::INPUT_MIN == -100);
-
-        //Importing max/min limits here
+        //Importing max/min limits here from config.hpp
         float yawMin = config::flightmodes[1].tuningData.controlLimits.yawLimit.min;
         float yawMax = config::flightmodes[1].tuningData.controlLimits.yawLimit.max;
 
@@ -21,11 +19,23 @@ namespace AM {
         float pitchMin = config::flightmodes[1].tuningData.controlLimits.pitchLimit.min;
         float pitchMax = config::flightmodes[1].tuningData.controlLimits.pitchLimit.max;
 
+        float throttleMin = config::flightmodes[1].tuningData.controlLimits.throttleLimit.min;
+        float throttleMax = config::flightmodes[1].tuningData.controlLimits.throttleLimit.max;
+
+        // Bounded checking to make sure input is within max and min
+        assert(input.pitch <= AM::AttitudeManager::INPUT_MAX && input.pitch >= AM::AttitudeManager::INPUT_MIN);
+        assert(input.roll <= AM::AttitudeManager::INPUT_MAX && input.roll >= AM::AttitudeManager::INPUT_MIN);
+        assert(input.yaw <= AM::AttitudeManager::INPUT_MAX && input.yaw >= AM::AttitudeManager::INPUT_MIN);
+        assert(input.throttle <= AM::AttitudeManager::INPUT_MAX && input.throttle >= AM::AttitudeManager::INPUT_MIN);
+
         // Mapping inputs to outputs with ratios of min-max
-        mappedOutputs.pitch = pitchMin + (input.pitch - AM::AttitudeManager::INPUT_MIN) * (pitchMax - pitchMin) / (AM::AttitudeManager::INPUT_MAX - AM::AttitudeManager::INPUT_MIN);
-        mappedOutputs.roll = rollMin + (input.roll - AM::AttitudeManager::INPUT_MIN) * (rollMax - rollMin) / (AM::AttitudeManager::INPUT_MAX - AM::AttitudeManager::INPUT_MIN);
-        mappedOutputs.yaw = yawMin + (input.yaw - AM::AttitudeManager::INPUT_MIN) * (yawMax - yawMin) / (AM::AttitudeManager::INPUT_MAX - AM::AttitudeManager::INPUT_MIN);
-        mappedOutputs.throttle = input.throttle;
+        // Mapping maintains 0->0 I/O
+        mappedOutputs.pitch = input.pitch < 0 ? input.pitch * (pitchMin /  AM::AttitudeManager::INPUT_MIN) : input.pitch * (pitchMax /  AM::AttitudeManager::INPUT_MAX);        
+        mappedOutputs.roll = input.roll < 0 ? input.roll * (rollMin /  AM::AttitudeManager::INPUT_MIN) : input.roll * (rollMax /  AM::AttitudeManager::INPUT_MAX);        
+        mappedOutputs.yaw = input.yaw < 0 ? input.yaw * (yawMin /  AM::AttitudeManager::INPUT_MIN) : input.yaw * (yawMax /  AM::AttitudeManager::INPUT_MAX);
+
+        // Throttle maps -100-100 to range 0-100
+        mappedOutputs.throttle = throttleMin + (input.throttle - AM::AttitudeManager::INPUT_MIN) * (throttleMax - throttleMin) / (AM::AttitudeManager::INPUT_MAX - AM::AttitudeManager::INPUT_MIN);
 
         return mappedOutputs;
     }
