04e593f4723c1974ba39bc323d24d7c80c027a99
alyzenjeraj <alyzenjeraj@gmail.com>
2024-04-02 19:29:52 -0400

    Fixed yawLimit typo

--- AttitudeManager/FlightModes/Src/fbwa.cpp
+++ AttitudeManager/FlightModes/Src/fbwa.cpp
@@ -18,7 +18,7 @@ namespace AM {
         // Mapping maintains 0->0 I/O
         mappedOutputs.pitch = input.pitch < 0 ? input.pitch * (fbwa_control_limits.pitchLimit.min /  AM::AttitudeManager::INPUT_MIN) : input.pitch * (fbwa_control_limits.pitchLimit.max /  AM::AttitudeManager::INPUT_MAX);        
         mappedOutputs.roll = input.roll < 0 ? input.roll * (fbwa_control_limits.rollLimit.min /  AM::AttitudeManager::INPUT_MIN) : input.roll * (fbwa_control_limits.rollLimit.max /  AM::AttitudeManager::INPUT_MAX);        
-        mappedOutputs.yaw = input.yaw < 0 ? input.yaw * (fbwa_control_limits.yawLimit.min /  AM::AttitudeManager::INPUT_MIN) : input.yaw * (fbwa_control_limits.rollLimit.max /  AM::AttitudeManager::INPUT_MAX);
+        mappedOutputs.yaw = input.yaw < 0 ? input.yaw * (fbwa_control_limits.yawLimit.min /  AM::AttitudeManager::INPUT_MIN) : input.yaw * (fbwa_control_limits.yawLimit.max /  AM::AttitudeManager::INPUT_MAX);
 
         // Throttle maps -100-100 to range 0-100
         mappedOutputs.throttle = fbwa_control_limits.throttleLimit.min + (input.throttle - AM::AttitudeManager::INPUT_MIN) * (fbwa_control_limits.throttleLimit.max - fbwa_control_limits.throttleLimit.min) / (AM::AttitudeManager::INPUT_MAX - AM::AttitudeManager::INPUT_MIN);
