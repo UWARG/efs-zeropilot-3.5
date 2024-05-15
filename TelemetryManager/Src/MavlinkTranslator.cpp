b1d5301044422b8cea484c8413cce38eed9bc44d
ydzulynsky <yarema.dzulynsky@gmail.com>
2024-05-14 20:44:42 -0400

    addMavlinkMsgToByteQueue notes

--- TelemetryManager/Src/MavlinkTranslator.cpp
+++ TelemetryManager/Src/MavlinkTranslator.cpp
@@ -23,5 +23,15 @@ void MavlinkTranslator::bytesToMavlinkMsg(TMCircularBuffer &rxFromGroundByteQueu
 void MavlinkTranslator::addMavlinkMsgToByteQueue(mavlink_message_t &msg, TMCircularBuffer &txToGroundByteQueue)
 {
     // Take Mavlink messages in txToGroundByteQueue and convert them to bytes
+    
+        
+        //WE NEED TO SOMEHOW GET THE MESSAGE TYPE TO USE THE CORRECT PACK FUNCTION
+        //
+        mavlink_msg_##baseName##_pack(0, 0, &msg, __VA_ARGS__);
+        std::size_t bufferLen = mavlink_msg_to_send_buffer(buffer, &msg);
+        
+        for (std::size_t i = 0; i < bufferLen; i++) {
+            txToGroundByteQueue.enqueue(buffer[i]);
+        }
 
 }
