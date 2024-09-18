8e3ca8f0c2cb3d5d73ca1ded136aa0143ef550a8
Anthony Luo <anthonyluo100@gmail.com>
2024-09-18 12:31:29 -0400

    Add tuning data constructor to FBWA

--- AttitudeManager/FlightModes/Inc/fbwa.hpp
+++ AttitudeManager/FlightModes/Inc/fbwa.hpp
@@ -2,12 +2,15 @@
 #define ZPSW3_AM_FBWA_HPP
 
 #include "flightmode.hpp"
+#include "PID.hpp"
+#include "config_foundation.hpp"
 
 namespace AM {
 
 class FBWA : public Flightmode {
    public:
-    FBWA() = default;
+    // FBWA() = default;
+    FBWA(const config::Flightmode_t& config) : tuningData(config.tuningData) {};
 
     AttitudeManagerInput run(const AttitudeManagerInput& input) override;
     void updatePid() override;
@@ -17,6 +20,7 @@ class FBWA : public Flightmode {
    private:
     // TODO: FIXME to be a control limit class
     ControlLimits_t fbwa_control_limits;
+    config::ControlTuning_t tuningData;
 };
 
 }  // namespace AM
