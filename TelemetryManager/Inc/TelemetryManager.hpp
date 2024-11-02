453e0bea5ccee85b9d2ba944a80a3f2af0a61021
larry-pan <l55pan@uwaterloo.ca>
2024-11-02 19:06:30 -0400

    bring up to date

--- TelemetryManager/Inc/TelemetryManager.hpp
+++ TelemetryManager/Inc/TelemetryManager.hpp
@@ -16,79 +16,122 @@
 #ifndef TELEMETRYMANAGER_H
 #define TELEMETRYMANAGER_H
 
+#define CREATE_GETTER(type, member) \
+    type get_##member() const { \
+        return (member != nullptr) ? *member : std::numeric_limits<type>::max(); \
+    }
+
+#define CREATE_SETTER(type, member) \
+    void set_##member(type* value) { member = value; }
+
 #include "FreeRTOS.h"
 #include "GroundStationCommunication.hpp"
 #include "MavlinkTranslator.hpp"
 #include "task.h"
+#include <limits>
 
 struct TMStateData {
+   private:
     /*References to variables that contain the state of the drone (lat, lng, yaw, pitch, etc..).
      * They are updated by System Manager*/
     // Altitude (MSL) (unit:
     // mm)(https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT).
-    int32_t* alt;
+    int32_t* alt = nullptr;
 
     // The latitude of the drone (unit:
     // degE7)(https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT).
-    int32_t* lat;
+    int32_t* lat = nullptr;
 
     // The longitude of the drone (unit:
     // degE7)(https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT).
-    int32_t* lon;
+    int32_t* lon = nullptr;
 
     // Altitude above home (unit:
     // mm)(https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT).
-    int32_t* relative_alt;
+    int32_t* relative_alt = nullptr;
 
     // Ground X Speed (Latitude, positive north) (unit:
     // cm/s)(https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT).
-    int16_t* vx;
+    int16_t* vx = nullptr;
 
     // Ground Y Speed (Longitude, positive east) (unit:
     // cm/s)(https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT).
-    int16_t* vy;
+    int16_t* vy = nullptr;
 
     // Ground Z Speed (Altitude, positive down) (unit:
     // cm/s)(https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT).
-    int16_t* vz;
+    int16_t* vz = nullptr;
 
     // Vehicle heading (yaw angle), 0.0..359.99 degrees. If unknown, set to: UINT16_MAX
     // (unit: cdeg)(https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT).
-    uint16_t* hdg;
+    uint16_t* hdg = nullptr;
     // Timestamp (time since system boot) (unit: ms)
     // (https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT).
-    int32_t* time_boot_ms;
+    int32_t* time_boot_ms = nullptr;
 
     // Roll angle (-pi..+pi) (unit: rad)
     // (https://mavlink.io/en/messages/common.html#ATTITUDE).
-    float* roll;
+    float* roll = nullptr;
 
     // Pitch angle (-pi..+pi) (unit: rad)
     // (https://mavlink.io/en/messages/common.html#ATTITUDE).
-    float* pitch;
+    float* pitch = nullptr;
 
     // Yaw angle (-pi..+pi) (unit: rad)
     // (https://mavlink.io/en/messages/common.html#ATTITUDE).
-    float* yaw;
+    float* yaw = nullptr;
 
     // Roll angular speed (unit: rad/s)
     // (https://mavlink.io/en/messages/common.html#ATTITUDE).
-    float* rollspeed;
+    float* rollspeed = nullptr;
 
     // Pitch angular speed (unit: rad/s)
     // (https://mavlink.io/en/messages/common.html#ATTITUDE).
-    float* pitchspeed;
+    float* pitchspeed = nullptr;
 
     // yawspeed Yaw angular speed (unit: rad/s)
     // (https://mavlink.io/en/messages/common.html#ATTITUDE).
-    float* yawspeed;
+    float* yawspeed = nullptr;
 
+   public:
+    // Generate getters using the macro
+    CREATE_GETTER(int32_t, alt)
+    CREATE_GETTER(int32_t, lat)
+    CREATE_GETTER(int32_t, lon)
+    CREATE_GETTER(int32_t, relative_alt)
+    CREATE_GETTER(int16_t, vx)
+    CREATE_GETTER(int16_t, vy)
+    CREATE_GETTER(int16_t, vz)
+    CREATE_GETTER(uint16_t, hdg)
+    CREATE_GETTER(int32_t, time_boot_ms)
+    CREATE_GETTER(float, roll)
+    CREATE_GETTER(float, pitch)
+    CREATE_GETTER(float, yaw)
+    CREATE_GETTER(float, rollspeed)
+    CREATE_GETTER(float, pitchspeed)
+    CREATE_GETTER(float, yawspeed)
+
+    CREATE_SETTER(int32_t, alt)
+    CREATE_SETTER(int32_t, lat)
+    CREATE_SETTER(int32_t, lon)
+    CREATE_SETTER(int32_t, relative_alt)
+    CREATE_SETTER(int16_t, vx)
+    CREATE_SETTER(int16_t, vy)
+    CREATE_SETTER(int16_t, vz)
+    CREATE_SETTER(uint16_t, hdg)
+    CREATE_SETTER(int32_t, time_boot_ms)
+    CREATE_SETTER(float, roll)
+    CREATE_SETTER(float, pitch)
+    CREATE_SETTER(float, yaw)
+    CREATE_SETTER(float, rollspeed)
+    CREATE_SETTER(float, pitchspeed)
+    CREATE_SETTER(float, yawspeed)
 };
 
 class TelemetryManager {
    public:
-
-    // struct containing variables that relate to the state of the drone (lat, lng, yaw, pitch, etc..).
+    // struct containing variables that relate to the state of the drone (lat, lng, yaw, pitch,
+    // etc..).
     TMStateData stateData;
 
     // the buffer that stores the bytes received from the ground station.
@@ -97,12 +140,10 @@ class TelemetryManager {
     // The object that facilitates communication with the ground station
     GroundStationCommunication& GSC;
 
-
     // System status flag (https://mavlink.io/en/messages/minimal.html#MAV_STATE).
     MAV_STATE& mavState;
     // System mode bitmap (https://mavlink.io/en/messages/minimal.html#MAV_MODE_FLAG).
     MAV_MODE_FLAG& mavMode;
-    
 
     /**
      * @brief Create and configure FreeRTOS tasks.
@@ -121,11 +162,12 @@ class TelemetryManager {
      * To do so call the init() method.
      * @param stateData The state of the drone (lat, lng, yaw, pitch, etc..).
      * @param mav_state System status flag (https://mavlink.io/en/messages/minimal.html#MAV_STATE).
-     * @param mav_mode System mode bitmap (https://mavlink.io/en/messages/minimal.html#MAV_MODE_FLAG).
+     * @param mav_mode System mode bitmap
+     * (https://mavlink.io/en/messages/minimal.html#MAV_MODE_FLAG).
      * @param GSC Object to handle communication with the groundstation
      * @param MT Object to translate MAVLink data
      */
-    TelemetryManager(TMStateData& stateData, MAV_STATE& mavState, MAV_MODE_FLAG& mavMode, 
+    TelemetryManager(TMStateData& stateData, MAV_STATE& mavState, MAV_MODE_FLAG& mavMode,
                      GroundStationCommunication& GSC, MavlinkTranslator& MT);
 
     /**
